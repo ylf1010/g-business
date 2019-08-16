b998b46d7b44fb1bc7224f3e36211090f1476ad6
ylf1010 <984005441@qq.com>
2019-08-16 16:10:30 +0800

    初始提交框架

---
+++ provider-ztx/src/main/java/com/jk/ProviderZtxApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderZtxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderZtxApplication.class, args);
+    }
+
+}
