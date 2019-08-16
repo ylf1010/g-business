836f4f2f83b813bc96fba9220f6b61c812f251f1
ztx630824378 <630824378@qq.com>
2019-08-16 19:29:41 +0800

    ztx 3

---
+++ common-api/src/main/java/com/jk/util/TreeNoteUtil.java
@@ -0,0 +1,52 @@
+package com.jk.util;
+
+import com.jk.model.ZtxTree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class TreeNoteUtil {
+    
+    /**
+     * 获取父节点菜单
+     * @param treesList 所有树菜单集合
+     * @return
+     */
+    public final static List<ZtxTree> getFatherNode(List<ZtxTree> treesList){
+        List<ZtxTree> newTrees = new ArrayList<ZtxTree>();
+        for (ZtxTree mt : treesList) {
+            if (mt.getPid() ==null || "".equals(mt.getPid()) || mt.getPid().equals("0")) {//如果pId为空，则该节点为父节点
+                //递归获取父节点下的子节点
+                mt.setNodes(getChildrenNode(mt.getId().toString(), treesList));
+                newTrees.add(mt);
+            }
+        }
+        return newTrees;
+    }
+    
+    /**
+     * 递归获取子节点下的子节点
+     * @param pId 父节点的ID
+     * @param treesList 所有菜单树集合
+     * @return
+     */
+    private final static List<ZtxTree> getChildrenNode(String pId, List<ZtxTree> treesList){
+        List<ZtxTree> newTrees = new ArrayList<ZtxTree>();
+        for (ZtxTree mt : treesList) {
+            if (mt.getPid()==null || mt.getPid().equals("0")) continue;
+            if(mt.getPid().equals(pId) ){
+                //递归获取子节点下的子节点，即设置树控件中的children
+                mt.setNodes(getChildrenNode(mt.getId().toString(), treesList));
+                //设置树控件attributes属性的数据
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("url", mt.getUrl());
+                mt.setAttributes(map);
+                newTrees.add(mt);
+            }
+        }
+        return newTrees;
+    }
+}
