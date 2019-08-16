0afa1e4e8d69d205d4a9b74d9b86aa030b5eedd9
ylf1010 <984005441@qq.com>
2019-08-16 16:33:20 +0800

    1提交框架

---
+++ common-api/src/main/java/com/jk/model/User.java
@@ -0,0 +1,83 @@
+package com.jk.model;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String baccount;
+
+    private Integer phone;
+
+    private String email;
+
+    private String status;
+
+    private Integer balanceid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getBaccount() {
+        return baccount;
+    }
+
+    public void setBaccount(String baccount) {
+        this.baccount = baccount == null ? null : baccount.trim();
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Integer getBalanceid() {
+        return balanceid;
+    }
+
+    public void setBalanceid(Integer balanceid) {
+        this.balanceid = balanceid;
+    }
+}
