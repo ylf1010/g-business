35b1efbc86d8a9f4b306aca743ab727d5e15e087
ztx630824378 <630824378@qq.com>
2019-08-16 19:38:02 +0800

    ztx 3

---
+++ consumer/src/main/java/com/jk/controller/ZtxController.java
@@ -0,0 +1,14 @@
+package com.jk.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.jk.service.ZtxService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ztx")
+public class ZtxController {
+
+    @Reference
+    private ZtxService zs;
+}
