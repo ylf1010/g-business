ec4c611e4c948cdfed40c9e667b2f18193df9fe3
ylf1010 <984005441@qq.com>
2019-08-16 21:24:01 +0800

    ylf提交登录

--- common-api/src/main/java/com/jk/service/LoginService.java
+++ common-api/src/main/java/com/jk/service/LoginService.java
@@ -1,4 +1,7 @@
 package com.jk.service;
 
+import com.jk.model.User;
+
 public interface LoginService {
+    User loginUser(User user);
 }
--- provider-ylf/src/main/java/com/jk/service/LoginServiceImpl.java
+++ provider-ylf/src/main/java/com/jk/service/LoginServiceImpl.java
@@ -2,7 +2,16 @@ package com.jk.service;
 
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.jk.dao.UserMapper;
+import com.jk.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class LoginServiceImpl implements LoginService{
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public User loginUser(User user) {
+        return userMapper.loginUser(user);
+    }
 }
