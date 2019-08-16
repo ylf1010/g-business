28ee1bd9f29b93d321eef9801f4922a8956537ff
ztx630824378 <630824378@qq.com>
2019-08-16 22:46:48 +0800

    张天祥0816最后一次提交

--- provider-ztx/src/main/java/com/jk/service/ZtxServiceImpl.java
+++ provider-ztx/src/main/java/com/jk/service/ZtxServiceImpl.java
@@ -1,9 +1,21 @@
 package com.jk.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.jk.dao.ZtxTreeMapper;
+import com.jk.model.ZtxTree;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Service
 public class ZtxServiceImpl implements ZtxService{
 
+    @Autowired
+    private ZtxTreeMapper tm;
 
+    @Override
+    public List<ZtxTree> querytree() {
+        System.out.println(tm.querytree());
+        return tm.querytree();
+    }
 }
