24f2d00315243654556e94cad036290fd19a8f78
lxx112323 <2693375436@qq.com>
2019-08-18 17:55:28 +0800

    添加地区

---
+++ common-api/src/main/java/com/jk/model/AreaBeanLxx.java
@@ -0,0 +1,33 @@
+package com.jk.model;
+
+public class AreaBeanLxx {
+    private Integer id;
+
+    private String areaname;
+
+    private Integer pid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAreaname() {
+        return areaname;
+    }
+
+    public void setAreaname(String areaname) {
+        this.areaname = areaname;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+}
