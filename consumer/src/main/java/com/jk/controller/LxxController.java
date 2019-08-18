639cf25865d2c7e7ee268e6f3cde3c47df397025
lxx112323 <2693375436@qq.com>
2019-08-18 11:35:22 +0800

    添加

--- consumer/src/main/java/com/jk/controller/LxxController.java
+++ consumer/src/main/java/com/jk/controller/LxxController.java
@@ -5,10 +5,13 @@ import com.jk.model.RenZhengBean;
 
 
 import com.jk.service.LxxService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("lxx")
@@ -53,4 +56,5 @@ public class LxxController {
             return false;
         }
     }
+
 }
