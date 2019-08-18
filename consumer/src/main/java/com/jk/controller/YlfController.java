2735f7983dc42f0af078b339d981dd0f276eb301
ylf1010 <984005441@qq.com>
2019-08-18 19:07:52 +0800

    ylf提交注册2

--- consumer/src/main/java/com/jk/controller/YlfController.java
+++ consumer/src/main/java/com/jk/controller/YlfController.java
@@ -4,7 +4,11 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.jk.model.User;
 import com.jk.service.LoginService;
 import com.jk.util.CheckImgUtil;
+import com.jk.util.DataGridResult;
+import com.jk.util.PageUtil;
+import com.jk.util.ParameUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,14 +22,20 @@ public class YlfController {
 
     @Reference
     private LoginService loginService;
-
+    //跳转登录页面
     @RequestMapping("tologinUser")
     public String tologinUser(){
         return "loginUser";
     }
+    //跳转注册页面
     @RequestMapping("toregister")
     public String toregister(){
         return "register";
+    }
+    //跳转账户总览页面
+    @RequestMapping("toaccountSum")
+    public String toaccountSum(){
+        return "accountSum";
     }
     //获取图片验证码
     @RequestMapping("getCode")
@@ -34,6 +44,7 @@ public class YlfController {
         CheckImgUtil.buildCheckImg(request, response);
 
     }
+    //商家登录
    @RequestMapping("loginUser")
     @ResponseBody
     public String loginUser(User user, HttpServletRequest request,String code){
@@ -53,6 +64,7 @@ public class YlfController {
          request.getSession().setAttribute("user",loginUser);
          return "success";
    }
+   //注册
    @RequestMapping("register")
     @ResponseBody
     public String reisterUser(User user){
@@ -65,6 +77,17 @@ public class YlfController {
 
         return "regissuccess";
    }
+
+    @RequestMapping("findaccountSum")
+    @ResponseBody
+    public DataGridResult cha(@RequestBody ParameUtil parame){
+        PageUtil pageUtil = loginService.findaccountSum(parame);
+        DataGridResult result = new DataGridResult();
+        result.setTotal(pageUtil.getSumSize());
+        result.setRows(pageUtil.getList());
+
+        return result;
+    }
 
 
 }
