f281cf48555d9520d7b8191ebe98fb6acb5662a6
lxx112323 <2693375436@qq.com>
2019-08-18 10:20:54 +0800

    添加

--- consumer/src/main/java/com/jk/ConsumerApplication.java
+++ consumer/src/main/java/com/jk/ConsumerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.jk.service"})
+
 public class ConsumerApplication {
 
     public static void main(String[] args) {
