3a68f2dc81c4177f98f7bee75e7ef3a33fbcf2a1
ysq123456789 <1198612861@qq.com>
2019-08-19 09:38:07 +0800

    ysq 交易  退款  评论

---
+++ common-api/src/main/java/com/jk/service/YsqService.java
@@ -0,0 +1,10 @@
+package com.jk.service;
+
+import com.jk.util.PageUtil;
+import com.jk.util.ParameUtil;
+
+public interface YsqService {
+    PageUtil cha(ParameUtil parame);
+
+    PageUtil cha2(ParameUtil parameUtil);
+}
